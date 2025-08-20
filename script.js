12ac419faa727a47aaeb39a26a1b88fabafb4c01
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2025-08-20 11:02:59 -0700

    Set Up Player and Game Modules
    Completed player, game controller, and game board modules.

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const gameBoard = (function () {
     let board = new Array(9).fill(null);
+    let boardCell = document.querySelector(".cell");
 
+    const getBoard = () => board;
 
+    const setCell = (index, marker) => {
+        if (board[index] == null) {
+            board[index] = marker;
+        }
+    };
 
-    return (board);
+    const resetBoard = () => {
+        board = new Array(9).fill(null);
+    };
+
+    return (getBoard, setCell, resetBoard);
 
 })();
 
@@ -22,5 +33,20 @@ const gameController = (function () {
     const player2 = Player("Ebner", "O");
 
     let currentPlayer = player1;
+
+    const playTurn = (index) => {
+        if (gameBoard.setCell(index, player1.indicator)) {
+            _switchTurn();
+        }
+    }
+
+    const _switchTurn = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    return { playTurn, getCurrentPlayer }
+
 })();
 
